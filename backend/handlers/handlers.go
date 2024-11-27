ad08202771b7ab4b282666ec0cda7cd120a1aecb
Garrett <garretth@tuta.io>
2024-11-27 11:52:53 -0500

    Remove unused handler

--- backend/handlers/handlers.go
+++ backend/handlers/handlers.go
@@ -2,28 +2,10 @@ package handlers
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
-	"os"
 
 	"linuxthekernel.io/helpers"
 )
-
-// takes the requested /blog/imgs/{id} path and transforms that to ./content/imgs/{id}
-// the reason for this is because the path is being modified by the react router.
-// this will probably die once this thing develops a little more
-func ImageHandler(w http.ResponseWriter, r *http.Request) {
-
-	imgPath := fmt.Sprintf("./content/imgs/%s", r.PathValue("id"))
-
-	if _, err := os.Stat(imgPath); err == nil {
-		http.ServeFile(w, r, imgPath)
-	} else if os.IsNotExist(err) {
-		http.NotFound(w, r)
-	} else {
-		http.Error(w, "Forbidden", http.StatusForbidden)
-	}
-}
 
 // returns the listing of all posts
 func PostsHandler(w http.ResponseWriter, r *http.Request) {
