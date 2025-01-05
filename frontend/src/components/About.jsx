1d45dd0e6b75d383a13d65e45d64abcc2b362ac8
Garrett <garretth@tuta.io>
2025-01-04 20:25:18 -0500

    Moved from local svg to remote image for render speeds

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import Fishing from "../../imgs/fishing.svg";
 
 function About() {
   return (
@@ -7,16 +6,13 @@ function About() {
       <p className="fs-1 text-center">About Me</p>
       <hr />
       <div className="row">
-        <div className="col-md-6">
-          <img
-            src={Fishing}
-            className="rounded mx-auto d-block img-thumbnail img-fluid"
-            alt="Fishing"
-          ></img>
-        </div>
-        <div className="col-md-6">
-          <p>
-            I'm Garrett Harvey. I am a DevOps & Software Engineer. This is a
+          <div className="col-md-6">
+              <img src="https://d17x1veniq4brh.cloudfront.net/fishing.jpg" className="img-fluid img-thumbnail p-3"
+                   alt="Fishing"/>
+          </div>
+          <div className="col-md-6">
+              <p>
+                  I'm Garrett Harvey. I am a DevOps & Software Engineer. This is a
             website that I made for fun. I intend to make this mostly about my
             technical work and personal projects but more than likely it'll just
             end up being blog posts about my time spent fishing.
